6d0ef65940c33c5714909438fd7e03273497988e
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2025-01-12 19:07:59 +0100

    Buttons Cart working

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,7 +1,18 @@
+import { useDispatch } from 'react-redux';
 import classes from './CartItem.module.css';
+import { cartActions } from '../../store/MyCart';
 
 const CartItem = (props) => {
   const { title, quantity, total, price } = props.item;
+  const dispatch = useDispatch();
+
+  function decrementHandler(title, price) {
+    dispatch(cartActions.removeItem({title, price}));
+  }
+
+  function incrementHandler(title, price) {
+    dispatch(cartActions.addItem({title, price}));
+  }
 
   return (
     <li className={classes.item}>
@@ -17,8 +28,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={() => decrementHandler(title, price)}>-</button>
+          <button onClick={() => incrementHandler(title, price)}>+</button>
         </div>
       </div>
     </li>
